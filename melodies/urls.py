282fa4e733e5090995004e79e1ff346cfaae144f
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-06-03 15:28:49 -0300

    Arquivos para deploy

--- melodies/urls.py
+++ melodies/urls.py
@@ -12,3 +12,4 @@ urlpatterns = [
 # media
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_ROOT, document_root=settings.STATIC_ROOT)
