a471692eb7add47cff66670f5db93405a33de091
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-06-03 15:31:42 -0300

    Arquivos para deploy

--- melodies/settings.py
+++ melodies/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["musical-lyrics.herokuapp.com/", "127.0.0.1:8000/"]
+ALLOWED_HOSTS = ["musical-lyrics.herokuapp.com", "127.0.0.1:8000"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
